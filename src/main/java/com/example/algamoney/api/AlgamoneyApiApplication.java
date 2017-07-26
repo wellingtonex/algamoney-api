70258ab37fd33ec82a34a7a17600469346817866
wellington.siqueira <wellingtonex@gmail.com>
2017-07-26 00:10:05 -0300

    refatorando nome de pacotes e criando a entidade categoria

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -1,4 +1,4 @@
-package com.algaworks.algamoneyapi;
+package com.example.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
