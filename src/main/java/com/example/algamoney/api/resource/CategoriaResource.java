596a0cd92f29b8c155f15e860a178c416b3a9369
wellington.siqueira <wellingtonex@gmail.com>
2017-07-26 23:03:57 -0300

    retornando  o Location do recurso criado no HttpServletResponse response

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
@@ -27,10 +31,13 @@ public class CategoriaResource {
 		List<Categoria> categorias = categoriaRepository.findAll();
 		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void salvar(@RequestBody Categoria categoria) {
+	public void salvar(@RequestBody Categoria categoria, HttpServletResponse response) {
 		categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoria.getCodigo())
+				.toUri();
+		response.setHeader("Location", uri.toASCIIString());
 	}
 }
