628860b16fc580d916412fc5afe2e231522936af
wellington.siqueira <wellingtonex@gmail.com>
2017-07-26 22:47:42 -0300

    retornando status response com @ResponseStatus

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -3,9 +3,13 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.model.Categoria;
@@ -22,5 +26,11 @@ public class CategoriaResource {
 	public ResponseEntity<?> listar() {
 		List<Categoria> categorias = categoriaRepository.findAll();
 		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void salvar(@RequestBody Categoria categoria) {
+		categoriaRepository.save(categoria);
 	}
 }
