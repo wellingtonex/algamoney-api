65f2a127417212b01fce864276d3da757f973b3c
wellington.siqueira <wellingtonex@gmail.com>
2017-07-29 01:20:12 -0300

    validando tamanho maximo e minino de nome com hibernate bean validator, exception handle e i18n

--- src/main/java/com/example/algamoney/api/model/Categoria.java
+++ src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
@@ -15,6 +16,7 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	@NotNull
+	@Size(min=3,max=50)
 	private String nome;
 
 	public Long getCodigo() {
