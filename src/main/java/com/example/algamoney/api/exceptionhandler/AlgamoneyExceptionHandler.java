d4e61a678c6e2a8e355e642484466dcb24658fe3
wellington.siqueira <wellingtonex@gmail.com>
2017-08-02 23:18:42 -0300

    implementando metodo responsavel por deletar uma pessoa pelo seu codigo e tratamento de erro com @ExceptionHandler({EmptyResultDataAccessException.class}) para o caso de se passar um codigo invalido

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -31,6 +33,14 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String descricao =  ex.getCause().getMessage();
 		List<Erro> erros = Arrays.asList(new Erro(mensagem, descricao));
 		return handleExceptionInternal(ex, erros , headers, status, request);
+	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(RuntimeException ex, WebRequest request) {
+		String mensagem = messageSource.getMessage("mensagem.recurso-nao-encontrado", null, LocaleContextHolder.getLocale());
+		String descricao =  ex.getMessage();
+		List<Erro> erros = Arrays.asList(new Erro(mensagem, descricao));
+		return handleExceptionInternal(ex, erros  , new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	@Override
